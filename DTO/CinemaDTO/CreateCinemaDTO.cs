bbc9f2898fc8d840ba5a9f71b7473825fdc46100
Dyonata <dyonatamachado95@gmail.com>
2022-01-18 23:08:27 -0300

    Segunda Correção em CreateCinemaDTO

--- DTO/CinemaDTO/CreateCinemaDTO.cs
+++ DTO/CinemaDTO/CreateCinemaDTO.cs
@@ -5,7 +5,8 @@ namespace FilmesApi.DTO.CinemaDTO
     public class CreateCinemaDTO
     {
         [Required(ErrorMessage = "O campo nome é obrigatório.")]
-        public string Nome { get; set; }     
+        public string Nome { get; set; }
+        public int GerenteId { get; set; }
         [Required(ErrorMessage = "O campo EnderecoId é obrigatório.")]
         public int EnderecoId { get; set; }
     }
